2e40b1ed0ee051bff54711f1c339da40f007f076
Amfishka <amfishprog@gmail.com>
2024-02-09 12:28:10 +0500

    обновление readme. Создание requirements. Добавлен отлов ошибок при генерации данных

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 DB_HOST="localhost"
+DB_PORT="5432"
 DB_NAME="dbname"
 DB_USER="myuser"
 DB_PASS="mypassword"
 
 class Config(object):
     SECRET_KEY = 'devkey'
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- generate_db.py
+++ generate_db.py
@@ -9,57 +9,76 @@ import json
 
 def table_Requisites_add_data():
     '''Заполнение таблицы с реквизитами'''
-    with open("app/tmp/table_Requisites.json", "r") as f:
-        data = json.load(f)
-    with app.app_context():
-        for d in data:
-            r = Requisites(full_name = d["full_name"],
-                    type_payment = d["type_payment"],
-                    type_card_or_account = d["type_card_or_account"],
-                    number_phone = d["number_phone"],
-                    limit = int(d["numberrange"]))
-            db.session.add(r)  
-        db.session.commit()
+    try:
+        with open("app/tmp/table_Requisites.json", "r") as f:
+            data = json.load(f)
+        with app.app_context():
+            for d in data:
+                r = Requisites(full_name = d["full_name"],
+                        type_payment = d["type_payment"],
+                        type_card_or_account = d["type_card_or_account"],
+                        number_phone = d["number_phone"],
+                        limit = int(d["numberrange"]))
+                db.session.add(r)  
+            db.session.commit()
+        print("Заполнение таблицы с реквизитами завершено")
+    except Exception as e:
+        print("Ощипка при Заполнении таблицы с реквизитами : ", e)
 
 
 def table_Request_payment_add_data():
     '''Заполнение таблицы с заявками на оплату'''
-    with open("app/tmp/table_Request_payment.json", "r") as f:
-        data = json.load(f)
-    with app.app_context(): 
-        for d in data:
-            r = Request_payment(summ = float(d["summ"]),
-                    requisites = int(d["requisites"]),
-                    status = d["status"])
-            db.session.add(r)
-        db.session.commit()
+    try:
+        with open("app/tmp/table_Request_payment.json", "r") as f:
+            data = json.load(f)
+        with app.app_context(): 
+            for d in data:
+                r = Request_payment(summ = float(d["summ"]),
+                        requisites = int(d["requisites"]),
+                        status = d["status"])
+                db.session.add(r)
+            db.session.commit()
+        print("Заполнение таблицы с заявками на оплату завершено")
+    except Exception as e:
+        print("Ощипка при Заполнении таблицы с заявками на оплату : ", e)
+
 
 def table_User_role():
     '''Заполнение таблицы ролей'''
-    with app.app_context():
-        role_admin = Role_user(name="admin")
-        role_user = Role_user(name="user")
-        db.session.add(role_admin)
-        db.session.add(role_user)
-        db.session.commit()
+    try:
+        with app.app_context():
+            role_admin = Role_user(name="admin")
+            role_user = Role_user(name="user")
+            db.session.add(role_admin)
+            db.session.add(role_user)
+            db.session.commit()
+        print("Заполнение таблицы ролей завершено")
+    except Exception as e:
+        print("Ощипка при Заполнении таблицы ролей : ", e)
+
 
 def table_User():
     '''Заполнение таблицы пользователей'''
-    with app.app_context():
-        # Создание админа
-        u_admin = User(name="admin")
-        u_admin.set_password('password')
-        u_admin.set_admin()
-        db.session.add(u_admin)
+    try:
+        with app.app_context():
+            # Создание админа
+            u_admin = User(name="admin")
+            u_admin.set_password('password')
+            u_admin.set_admin()
+            db.session.add(u_admin)
 
-        # Создание пользователя
-        u_user = User(name="andry")
-        u_user.set_password('password')
-        db.session.add(u_user)
+            # Создание пользователя
+            u_user = User(name="andry")
+            u_user.set_password('password')
+            db.session.add(u_user)
 
-        db.session.commit()
+            db.session.commit()
+        print("Заполнение таблицы пользователей завершено")
+    except Exception as e:
+        print("Ощипка при Заполнении таблицы пользователей : ", e)
 
 if __name__ == "__main__":
+    print("Заполняем таблицы")
     table_Requisites_add_data()
     table_Request_payment_add_data()
     table_User_role()
